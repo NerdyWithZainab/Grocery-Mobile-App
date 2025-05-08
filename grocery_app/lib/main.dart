ee7c7961fd9494f0341b217c28f68feb0539bef3
Zainab Rangwala <zfr2000@hw.ac.uk>
2025-05-08 12:42:09 +0530

    Made the screens correspond with the screen index

--- grocery_app/lib/main.dart
+++ grocery_app/lib/main.dart
@@ -44,63 +44,93 @@ class Grocery extends StatefulWidget {
 }
 
 class _GroceryState extends State<Grocery> {
+  int screenIndex = 0;
+  final screens = [
+    const HomeScreen(),
+    const CategoriesScreen(),
+    const CartScreen(),
+    const LocationScreen(),
+    const UserProfileScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: Container(
-        height: 60,
-        decoration: BoxDecoration(
-          color: Colors.white70,
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20),
-            topRight: Radius.circular(20),
+    return Scaffold(bottomNavigationBar: NavBar(context));
+  }
+
+  Container NavBar(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: Colors.white70,
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                screenIndex = 0;
+              });
+            },
+            icon: const Icon(
+              Icons.home_outlined,
+              color: Colors.black,
+              size: 35,
+            ),
           ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.home_outlined,
-                color: Colors.black,
-                size: 35,
-              ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                screenIndex = 1;
+              });
+            },
+            icon: const Icon(
+              Icons.shopping_bag_outlined,
+              color: Colors.black,
+              size: 35,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.shopping_bag_outlined,
-                color: Colors.black,
-                size: 35,
-              ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                screenIndex = 2;
+              });
+            },
+            icon: const Icon(
+              Icons.shopping_cart_outlined,
+              color: Colors.black,
+              size: 35,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.shopping_cart_outlined,
-                color: Colors.black,
-                size: 35,
-              ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                screenIndex = 3;
+              });
+            },
+            icon: const Icon(
+              Icons.location_on_outlined,
+              color: Colors.black,
+              size: 35,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.location_on_outlined,
-                color: Colors.black,
-                size: 35,
-              ),
+          ),
+          IconButton(
+            onPressed: () {
+              setState(() {
+                screenIndex = 4;
+              });
+            },
+            icon: const Icon(
+              Icons.person_2_outlined,
+              color: Colors.black,
+              size: 35,
             ),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.person_2_outlined,
-                color: Colors.black,
-                size: 35,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -142,6 +172,20 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class LocationScreen extends StatefulWidget {
+  const LocationScreen({super.key});
+
+  @override
+  State<LocationScreen> createState() => _LocationScreenState();
+}
+
+class _LocationScreenState extends State<LocationScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
